b77fef16309d4545f9d911ac6209a509595ae823
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-11-18 12:34:22 +0700

    Add Services & Interfaces

---
+++ RookieOnlineAssetManagement/Services/ReturnRequest.cs
@@ -0,0 +1,13 @@
+﻿using RookieOnlineAssetManagement.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Services
+{
+    public class ReturnRequest : IReturnRequest
+    {
+
+    }
+}
